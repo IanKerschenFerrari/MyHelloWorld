d675e2c5c38f0b9ae3928b33db6ea0a47538e18c
Ian Kerschen Ferrari <kerschen.ian@gmail.com>
2021-07-14 21:06:28 -0300

    Add files via upload

---
+++ plantas.py
@@ -0,0 +1,16 @@
+plantas = input("Inserte el nombre de la planta: ")
+if plantas == "zapallo":
+    print("Septiembre/Noviembre__Prof 2/3cm__Dist 1m__3/6 meses")
+if plantas == "espinaca":
+    print("Prof 1/2cm__Distancia 10/15cm")
+if plantas == "escarola":
+    print("Abr/sep__Prof 0,5cm__Dist 30cm__50/60 dias")
+if plantas == "ciboulette":
+    print("Todo el ano")
+if plantas == "repollo":
+    print("Prof 0,5/1cm__Dist 30/40cm__3/4 meses")
+if plantas == "papa":
+    print("Primavera__Prof 8cm__Dist 30-50cm__4 meses")
+if plantas == "boniato":
+    print("Primavera__Prof 5/10cm__6 meses__1:Cortar el boniato al medio__2:Ponerlo con el corte 2 o 3cm abajo de agua__3:Ponerlo en un lugar calido y luminoso__4:Cuando los brotes tengan entre 10 y 15cm de largo hay que arrancarlos tirando__5:A los brotes que sacamos ahora hay que ponerlos en agua, esperar una semana y transplantarlos a tierra.")
+input("Toque enter para salir")
